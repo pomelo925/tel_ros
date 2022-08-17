4a3769d0b7989532afc20383477b31ecf8d35ada
pomelo925 <yoseph.huang@gmail.com>
2022-08-17 17:41:23 +0800

    update

---
+++ build/race/CMakeFiles/aaatest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aaatest.dir/src/aaatest.cpp.o"
+  "aaatest"
+  "aaatest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aaatest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/race/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pomelo925/POMELO/TEL/src/race
+# Build directory: /home/pomelo925/POMELO/TEL/build/race
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
