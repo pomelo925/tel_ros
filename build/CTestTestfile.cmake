0656caca277bc747d5522b69753a8bbcb7c94428
pomelo925 <82867625+pomelo925@users.noreply.github.com>
2022-11-02 21:53:16 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pomelo925/POMELO/TEL/src
-# Build directory: /home/pomelo925/POMELO/TEL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("remote")
